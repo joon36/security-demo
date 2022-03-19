0e9db8ae7230585f5bedd48c70119ba93ec27172
joon36 <kwjoon7@gmail.com>
2022-03-19 20:54:30 +0900

    인가 과정 설명

--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -32,3 +32,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 // WebSecurityConfigurerAdapter : 스프링 시큐리티의 웹 보안 기능 설정
 // HttpSecurity : 세부적인 보안 기능을 설정
 // 인증 API & 인가 API 를 구분하여 작성
+
+// AuthenticationManager : (인증) 인증을 수행하는 매니저
+// AccessDecisionManager : (인가) 특정 서비스의 리소스에 접근할 때, 허용할 것인가? 유효한 요청인가? 를 결정하는 매니저
+// 1. AffirmativeBased : 하나의 Voter 라도 허용하면 허용 (default)
+// 2. ConsensusBased : 다수결
+// 3. UnanimousBased : 만장 일치
+
+// FilterSecurityInterceptor
+// -> AccessDecisionManager를 사용하여 Access Control 또는 예외 처리를 하는 필터
